fb59bd89a7a70113d2da627b1b149f08c749b995
Krzysztof Boronski <kboronski@antmicro.com>
2022-10-20 18:19:09 +0200

    Add license headers
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- src/include_path.rs
+++ src/include_path.rs
@@ -1,3 +1,19 @@
+/* Copyright (C) 2022 Antmicro
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 /* Note:
  * This breaks cross-compilation. An alternative trick is to check the `cfg`
  * in `build.rs`, set `cargo:rust-cfg=` based on that and use that here, but this
